d48cfcc535da41404774520e0cc76b5689b51fdc
manish-107 <creativecrew2676@gmail.com>
2024-08-17 20:57:40 +0530

    landing page

--- frontXpay/app/(auth)/signup.jsx
+++ frontXpay/app/(auth)/signup.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Text, View, Image, Dimensions } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const signup = () => {
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="h-full bg-[#000000]">
+      <View className="flex-1 p-4 mt-4 ">
+        <View className="flex flex-col justify-around p-6 mb-4 bg-[#56545454] rounded-3xl">
+          <Text className="text-2xl font-bold text-white">Login</Text>
+          <Link
+            style={{ textDecorationLine: "none" }}
+            className="text-xl font-bold bg-[#CBCF00] text-[#000000] rounded-3xl text-center px-8 py-3 underline"
+            href="/signup"
+          >
+            GetStarted
+          </Link>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default signup
+export default signup;
--- frontXpay/app/index.jsx
+++ frontXpay/app/index.jsx
@@ -1,44 +1,54 @@
-import { ScrollView, Text, View } from "react-native";
 import React from "react";
+import { Text, View, Image, Dimensions } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import indeximg from "../assets/indeximg2.png";
 
-const index = () => {
+const Index = () => {
+  // Get the screen height
+  const { height } = Dimensions.get("window");
+
   return (
-    <>
-      <SafeAreaView className="h-full bg-[#f7f7f7]">
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <View className="justify-between flex-1 p-6 mt-8">
-            <View>
-              <Text className="pb-3 text-4xl font-extrabold text-white">
-                Pay<Text className="text-[#00C2C7]">X</Text>wise
-              </Text>
-            </View>
-            <View className="flex flex-col justify-around p-5 mt-10 bg-[#161614] rounded-3xl">
-              <Text className="text-3xl font-medium pt-3 leading-relaxed text-[#b3b3b3]">
-                Simplify{" "}
-                <Text className="text-[#F1F469] font-bold">transaction </Text>{" "}
-                and
-                <Text className="text-[#F1F469] font-bold"> expense</Text>{" "}
-                management
-              </Text>
-              <Text className="py-6 text-lg text-[#CBCBC7] ">
-                Here's a styled version of your component with the desired
-                changes
-              </Text>
-              <Link
-                style={{ textDecorationLine: "none" }}
-                className="text-xl font-bold bg-[#CBCF00] text-[#000000] rounded-3xl text-center px-8 py-3 underline"
-                href="/signup"
-              >
-                GetStarted
-              </Link>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView className="h-full bg-[#1c1d1d]">
+      <View className="justify-around flex-1 p-4 mt-4">
+        <View>
+          <Image
+            source={indeximg}
+            style={{
+              width: "100%",
+              height: height * 0.5, // Adjust the height to 30% of screen height
+
+              marginHorizontal: 10, // Add horizontal margin (left and right)
+              padding: 0,
+            }}
+            resizeMode="contain" // Ensure the image maintains its aspect ratio
+          />
+        </View>
+        <View className="flex flex-col justify-around p-6 mb-4 bg-[#161614] rounded-3xl">
+          <Text className="text-4xl font-extrabold text-white ">
+            Pay<Text className="text-[#00C2C7]">X</Text>wise
+          </Text>
+          <Text className="text-3xl font-medium pt-3 leading-relaxed text-[#b3b3b3]">
+            Simplify{" "}
+            <Text className="text-[#F1F469] font-bold">transaction </Text>
+            and
+            <Text className="text-[#F1F469] font-bold"> expense</Text>{" "}
+            management
+          </Text>
+          <Text className="py-6 text-lg text-[#CBCBC7]">
+            Here's a styled version of your component with the desired changes
+          </Text>
+          <Link
+            style={{ textDecorationLine: "none" }}
+            className="text-xl font-bold bg-[#CBCF00] text-[#000000] rounded-3xl text-center px-8 py-3 underline"
+            href="/signup"
+          >
+            GetStarted
+          </Link>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default index;
+export default Index;
