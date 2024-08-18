c5ba6c52db77c1a8904246548f8f7795ec0a1cba
manish-107 <creativecrew2676@gmail.com>
2024-08-18 12:24:25 +0530

    signup form design

--- frontXpay/app/(auth)/_layout.jsx
+++ frontXpay/app/(auth)/_layout.jsx
@@ -5,6 +5,21 @@ const authLayout = () => {
     <Stack>
       <Stack.Screen name="signin" options={{ headerShown: false }} />
       <Stack.Screen name="signup" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="signupnext"
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#000000", // Background color of the header
+          },
+          headerTintColor: "#fcf955", // Color of the header text and icons
+          headerTitleStyle: {
+            fontWeight: "bold", // Style the header title text
+            fontSize: 24,
+          },
+          headerTitle: "Sign In", // Custom header title
+        }}
+      />
     </Stack>
   );
 };
--- frontXpay/app/(auth)/signupnext.jsx
+++ frontXpay/app/(auth)/signupnext.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import { Text, View, TextInput } from "react-native";
 import { Link } from "expo-router";
+import React from "react";
+import { Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const signup = () => {
+const signupnext = () => {
   return (
     <SafeAreaView className="h-full bg-[#000000]">
       <View className="flex-1 p-4 mt-4 ">
         <View className="flex flex-col justify-around p-6 mb-4 bg-[#00000054] rounded-3xl">
-          <Text className="text-2xl font-bold text-[#fcf955]">
-            Create Account
-          </Text>
           <View className="mt-6">
             <View className="p-1 mb-3">
               <Text className="p-1 text-xl font-semibold text-white">
-                Full Name
+                Date of birth
+              </Text>
+
+              <TextInput
+                className="w-full h-10 text-white bg-[#161614] rounded-lg "
+                placeholder="Enter text"
+                // value={""}
+                // onChangeText={""}
+              />
+            </View>
+            <View className="p-1 mb-3">
+              <Text className="p-1 text-xl font-semibold text-white">
+                Gender
               </Text>
               <TextInput
                 className="w-full h-10 text-white bg-[#161614] rounded-lg "
@@ -25,7 +34,7 @@ const signup = () => {
             </View>
             <View className="p-1 mb-3">
               <Text className="p-1 text-xl font-semibold text-white">
-                Email
+                Security Questions
               </Text>
               <TextInput
                 className="w-full h-10 text-white bg-[#161614] rounded-lg "
@@ -33,11 +42,6 @@ const signup = () => {
                 // value={""}
                 // onChangeText={""}
               />
-            </View>
-            <View className="p-1 mb-3">
-              <Text className="p-1 text-xl font-semibold text-white">
-                Phone number
-              </Text>
               <TextInput
                 className="w-full h-10 text-white bg-[#161614] rounded-lg "
                 placeholder="Enter text"
@@ -45,31 +49,9 @@ const signup = () => {
                 // onChangeText={""}
               />
             </View>
-            <View className="p-1 mb-3">
-              <Text className="p-1 text-xl font-semibold text-white">
-                Password
-              </Text>
-              <TextInput
-                className="w-full h-10 text-white bg-[#161614] rounded-lg "
-                placeholder="Enter text"
-                // value={""}
-                // onChangeText={""}
-              />
-            </View>
-            <View className="p-1 mb-1">
-              <Text className="p-1 text-xl font-semibold text-white">
-                Confirm password
-              </Text>
-              <TextInput
-                className="w-full h-10 text-white bg-[#161614] rounded-lg "
-                placeholder="Enter text"
-                // value={""}
-                // onChangeText={""}
-              />
-            </View>
+
             <Text className="flex p-5 mb-4 text-lg font-light text-center text-white">
-              Dont have an account{" "}
-              <Text className="font-light text-blue-600">Signup</Text>
+              terms and condition
             </Text>
           </View>
           <Link
@@ -77,7 +59,7 @@ const signup = () => {
             className="text-xl font-bold bg-[#CBCF00] text-[#000000] rounded-3xl text-center px-8 py-3 underline"
             href="/signupnext"
           >
-            {`Next ->`}
+            {`Create account`}
           </Link>
         </View>
       </View>
@@ -85,4 +67,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default signupnext;
